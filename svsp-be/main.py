d837ef0785c4a753189217a2bda361e0a92b0c44
Moderator11 <p7925221@gmail.com>
2025-09-21 02:09:53 +0900

    feature: Back-end project setup
    - FastAPI base code setup

---
+++ svsp-be/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from api.routes import healthcheck
+
+app = FastAPI(
+    title="SVSP FastAPI Service",
+    description="Semantic Video Summarization Pipeline Backend API documentation",
+    version="1.0.0"
+)
+
+app.include_router(healthcheck.router)
+
+@app.get("/", tags=["Root"])
+async def root():
+    return {"message": "Hello World"}
